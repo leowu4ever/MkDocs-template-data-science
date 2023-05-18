b0e873fbfb68c6fabf2c8de30a23345464932360
Leo Wu <leowu4ever@users.noreply.github.com>
2023-05-18 17:42:17 +0000

    - Got all demo module ready

--- src/car.py
+++ src/car.py
@@ -1,6 +1,28 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    def __init__(self, num_of_wheels):
+    '''
+    A car class which inherits from Vehicle.
+    '''
+    def __init__(self, num_of_wheels: int, brand: str):
+        '''
+        Initialise a car with the given number of wheels.
+
+        Args:
+            num_of_wheels (int): the number of wheels a vehicle has
+        '''
         super().__init__(num_of_wheels)
+        self._brand = brand
+        
+        
+    @property
+    def brand(self) -> str:
+        '''
+        Return the brand of the car.
+
+        Returns:
+            str: the brand of the car
+        '''
+        return self._brand
+    
 
--- src/vehicle.py
+++ src/vehicle.py
@@ -1,20 +1,26 @@
 class Vehicle():
-    
+    '''
+    This is a vehicle class. It has a number of wheels.
+    '''
+
     def __init__(self, num_of_wheels: int):
         '''
         To initialise a vehicle by supplying the number of wheels
 
         Args:
-            num_of_wheels (int): the number of wheels a vehicle can have
+            num_of_wheels (int): number of wheels a vehicle has
         '''
         self._num_of_wheels = num_of_wheels
-    
+
     @property
     def num_of_wheels(self) -> int:
         '''
         To access the number of wheels a vehicle can have
 
         Returns:
-            int: number of wheels specified
+            int: the number of wheels specified
         '''
         return self._num_of_wheels
+
+
+\\\\\\\--==='';';',...,,......
